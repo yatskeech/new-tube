dec5fe3f5e4126098827750e2242ce56ac6a1a2b
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-10-04 23:39:48 +0300

    feat: connect to db

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    clerkId: text('clerk_id').unique().notNull(),
+    name: text('name').notNull(),
+    imageUrl: text('image_url').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+  },
+  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+);
